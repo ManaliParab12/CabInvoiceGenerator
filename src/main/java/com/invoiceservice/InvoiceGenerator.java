cefa85abcb39cdb229a7d93e1ab8e474f276645c
ManaliParab12 <manaliparab10@gmail.com>
2020-12-14 16:09:08 +0530

    Added calculateFare method to generate invoice of total fare

--- src/main/java/com/invoiceservice/InvoiceGenerator.java
+++ src/main/java/com/invoiceservice/InvoiceGenerator.java
@@ -1,5 +1,9 @@
 package com.invoiceservice;
 
 public class InvoiceGenerator {
-
+    private static final int costPerTime = 1;
+    private static final double minimumCostPerKilometer = 10.0;
+    public double calculateFare(double distance, int time) {
+        return distance * minimumCostPerKilometer + time * costPerTime;
+    }
 }
