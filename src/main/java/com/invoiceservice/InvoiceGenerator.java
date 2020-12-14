67b455f4ae910c9c5cc2af44e2b36991d1a3fbca
ManaliParab12 <manaliparab10@gmail.com>
2020-12-14 16:17:27 +0530

    Added calculateFare method to calculate minimum fare

--- src/main/java/com/invoiceservice/InvoiceGenerator.java
+++ src/main/java/com/invoiceservice/InvoiceGenerator.java
@@ -3,7 +3,12 @@ package com.invoiceservice;
 public class InvoiceGenerator {
     private static final int costPerTime = 1;
     private static final double minimumCostPerKilometer = 10.0;
+    private static final double minimumFare = 5;
+
     public double calculateFare(double distance, int time) {
-        return distance * minimumCostPerKilometer + time * costPerTime;
+        double totalFare =  distance * minimumCostPerKilometer + time * costPerTime;
+        if(totalFare < minimumFare)
+            return minimumFare;
+        return totalFare;
     }
 }
--- src/test/java/com/invoiceservice/InvoiceServiceTest.java
+++ src/test/java/com/invoiceservice/InvoiceServiceTest.java
@@ -20,6 +20,6 @@ public class InvoiceServiceTest {
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
-        Assert.assertEquals(5, fare);
+        Assert.assertEquals(5, fare, 0.0);
     }
 }
